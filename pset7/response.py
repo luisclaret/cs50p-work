7ebc9b947caca0ace2dd5e4828dc7ca9e72fdb96
luisclaret <luisclaretafanador@gmail.com>
2026-08-13 10:30:32 -0500

    Problem set 7 completed

--- pset7/response.py
+++ pset7/response.py
@@ -1,8 +1,11 @@
+from validator_collection import validators, checkers, errors
+
 def main():
     print(checkmail(input("What is your email address?: ")))
 
 def checkmail(s):
-    ...
+    is_email_address = checkers.is_email(s)
+    return "Valid" if is_email_address else "Invalid"
 
 if __name__ == "__main__":
     main()
